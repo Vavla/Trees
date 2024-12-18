5ec3b54495ec0ba3cc08bd20087cb7057696ba57
Vavla <vlasovalera05@gmail.com>
2024-12-19 00:26:21 +1000

    Add initialization node

--- RedBlackTree/RedBlackTree.cpp
+++ RedBlackTree/RedBlackTree.cpp
@@ -3,18 +3,41 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+typedef struct Node {
+    Node* parent;
+    int day;
+    int mouth;
+    int year;
+    Node* left;
+    Node* right;
+    int blackHeight;
+    bool isBlack;
+} Node;
+
+void initialization(Node* &node) {
+
+    std::cin >> node->day;
+    std::cin >> node->mouth;
+    std::cin >> node->year;
+
+    node->parent = nullptr;
+    node->blackHeight = 0;
+    node->right = nullptr;
+    node->left = nullptr;
+    node->isBlack = 1;
+
+
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
+int main()
+{
+    Node* node = new Node;
+    initialization(node);
+    std::cout << node->day;
+    std::cout << node->isBlack;
+}
+
